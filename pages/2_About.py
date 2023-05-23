95d24e55faee364eded1a95199e3162528226758
Mohammed-Amaan <63122937+Mohammed-Amaan@users.noreply.github.com>
2023-05-23 23:53:26 +0400

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -3,13 +3,24 @@ import streamlit as st
 st.set_page_config(page_title="CareerBoost", page_icon="🚀")
 st.markdown("<h1 style='color: #d66d22;'>About Us</h1>", unsafe_allow_html=True)
 
-st.write(
-    "Our application is designed to revolutionize the job search process by leveraging the power of ChatGPT API. By "
-    "analyzing the skills extracted from a user's CV, our platform generates personalized job recommendations that "
-    "align with their career aspirations. But that's not all - we also provide detailed roadmaps that outline the "
-    "skills and experiences necessary to achieve their desired role, as well as potential internships and job "
-    "opportunities along the way. Whether you're looking to switch careers or acquire new skills to advance in your "
-    "current field, our platform is the perfect tool to help you achieve your goals. With its intuitive interface and "
-    "advanced algorithms, our application is the ultimate resource for anyone seeking a fulfilling and rewarding "
-    "career."
-)
+st.write("""
+Welcome to our revolutionary job search platform! Our application is designed to transform the way you navigate the job market by harnessing the power of the ChatGPT API. With our advanced algorithms and intuitive interface, we are here to guide you towards a fulfilling and rewarding career.
+
+**Resume Page:**
+The first step on your journey begins with our Resume Page. Here, you can upload your CV or resume, and our platform will analyze it to extract your skills. This powerful feature allows us to gain deep insights into your expertise and career background.
+
+Based on the skills extracted from your resume, our platform generates personalized job recommendations that align with your career aspirations. Gone are the days of manually searching through countless job listings. We streamline the process by presenting you with relevant opportunities tailored to your unique skill set.
+
+**Summarizer Page:**
+Our Summarizer Page takes job descriptions to the next level. Simply paste a job description into our platform, and it will provide you with a concise summary of the key requirements and responsibilities. This time-saving feature enables you to quickly assess whether a particular job aligns with your career goals.
+
+**Roadmaps:**
+But we don't stop at job recommendations. We understand that achieving your desired role may require specific skills and experiences. That's why our platform goes the extra mile by providing detailed roadmaps. These roadmaps outline the skills and experiences necessary to reach your desired career destination.
+
+Whether you're looking to switch careers or advance in your current field, our roadmaps act as your guide, highlighting the milestones you need to achieve along the way. These actionable steps ensure that you stay on track and make progress towards your ultimate career goal.
+
+**Internships and Job Opportunities:**
+We know that internships and early career opportunities are crucial for building a strong foundation. Our platform identifies potential internships and job openings that can help you gain the necessary experience and launch your career in the right direction.
+
+By connecting you with these valuable opportunities, we provide you with a head start on your career path. Whether you're a recent graduate or looking to make a transition, our platform equips you with the tools and resources needed to succeed.
+""")
